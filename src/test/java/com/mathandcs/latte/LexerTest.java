b84cb41b26acfdf233db92287d0882d300a91986
wusuopubupt <wdxbupt2009@gmail.com>
2017-07-31 17:20:48 +0800

    lexer and corresponding ut

--- src/test/java/com/mathandcs/latte/LexerTest.java
+++ src/test/java/com/mathandcs/latte/LexerTest.java
@@ -1,10 +1,18 @@
 package com.mathandcs.latte;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import com.mathandcs.latte.gui.CodeDialog;
 
 /**
- * Created by wangdongxu on 7/31/17.
+ * Created by dashwang on 7/31/17.
  */
 public class LexerTest {
 
+    @Test
+    public void testRead() throws ParseException {
+        Lexer l = new Lexer(new CodeDialog());
+        for (Token t; (t = l.read()) != Token.EOF; )
+            System.out.println("=> " + t.getText());
+    }
 }
+
